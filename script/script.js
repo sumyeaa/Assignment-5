8dedd48ed899e318f372cf08c5ef8081300ed9b1
sumyeaa <sumu200307@gmail.com>
2026-03-09 23:57:53 +0600

    modal added

--- script/script.js
+++ script/script.js
@@ -20,6 +20,85 @@ const mainpage=()=>{
 //     alert("Wrong username or password.Please try again.")
 // }
 }
+//modal part
+
+const loadmodal=async(id)=>{
+    const url=`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res=await fetch(url);
+    const details=await res.json();
+    displaymodal(details.data);
+};
+const displaymodal=(word)=>{
+    const detailsbox=document.getElementById("details_container");
+
+    //label button part
+         let labelBtn="";
+
+    for (let label of word.labels) {
+        let labelLower = label.toLowerCase();
+      if (labelLower==="good first issue") {
+        continue;
+      }
+      else if (labelLower==="enhancement") {
+        labelBtn+=`
+        <button class="btn btn-soft btn-success border border-green-500 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+      else if(labelLower==="bug"){
+        labelBtn+=`
+        <button class="btn btn-soft btn-secondary border border-pink-700 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+      else if(labelLower==="documentation"){
+        labelBtn+=`
+        <button class="btn btn-soft btn-info border border-blue-400 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+      else{
+        labelBtn+=`
+        <button class="btn btn-soft btn-warning border border-yellow-700 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+    }
+
+    //modalhtml
+
+    detailsbox.innerHTML=`
+    
+     <h3 class="text-2xl font-medium">${word.title}</h3>
+        <div class=" flex gap-4">
+            <p class="font-thin">Opened by</p>
+            <p>${word.assignee}</p>
+            <p> - <span>${word.updatedAt}</span></p>
+        </div>
+        <div>
+            <button>${labelBtn}</button>
+        </div>
+        <p>${word.description}</p>
+        <div class="grid grid-cols-2 bg-gray-100 p-5">
+            <div class="space-y-2">
+                <p>Assigned</p>
+                <h3 class=" text-xl font-medium">
+                ${word.assignee?word.assignee:"not found"}
+                </h3>
+            </div>
+            <div class="space-y-2">
+                <p>Priority</p>
+                <button class="bg-red-800 rounded-full w-[100px] p-3 text-white">${word.priority}</button>
+            </div>
+        </div>
+    
+    `
+    document.getElementById("word_modal").showModal();
+}
 
 let allIssue=[];
 const loadData=()=>{
@@ -102,7 +181,7 @@ const displaydata=(allbtns)=>{
       //html added
         const all=document.createElement("div");
         all.innerHTML=`
-           <div class="bg-slate-100 h-full flex flex-col max-w-[350px] md:max-w-[300px] lg:max-w-[256px] p-5 rounded-xl space-y-4 mx-auto mt-3 ${cardborder}">
+           <div onclick="loadmodal(${allbtn.id})" class="bg-slate-100 h-full flex flex-col max-w-[350px] md:max-w-[300px] lg:max-w-[256px] p-5 rounded-xl space-y-4 mx-auto mt-3 ${cardborder}">
             <div class="flex justify-end">
                 ${priorityBtn}
             </div>
@@ -112,8 +191,8 @@ const displaydata=(allbtns)=>{
               <div class="flex gap-3 mb-4 flex-wrap">${labelBtn}</div>
             </div>
             <hr class="text-gray-300 mb-5">
-            <p class="text-gray-400 text-xs mb-3">#1 by john_doe</p>
-            <p class="text-gray-400 text-xs">1/15/2024</p>
+            <p class="text-gray-400 text-xs mb-3">${allbtn.author}</p>
+            <p class="text-gray-400 text-xs">${allbtn.createdAt}</p>
         </div>
         `
         allDiv.append(all);
