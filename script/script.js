13ca31c1af9dae57ef5dd680f846e24475e9de99
sumyeaa <sumu200307@gmail.com>
2026-03-09 22:47:44 +0600

    button & card

--- script/script.js
+++ script/script.js
@@ -5,11 +5,11 @@ const pass = document.getElementById("pass");
 const signinBtn = document.getElementById("signin_btn");
 const newissue = document.getElementById("newissue");
 const search = document.getElementById("search");
-if(username === "admin" && pass === "admin123")
-{
-    loginPart.classList.add.hidden;
-    mainPart.classList.remove.hidden;
-}
+const closed=document.getElementById("closed");
+const open=document.getElementById("open");
+const all=document.getElementById("all");
+
+
 const mainpage=()=>{
 //     if(username.value === "admin" && pass.value === "admin123")
 // {
@@ -20,3 +20,127 @@ const mainpage=()=>{
 //     alert("Wrong username or password.Please try again.")
 // }
 }
+
+let allIssue=[];
+const loadData=()=>{
+    all.classList.add("activecolour")
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then((res)=>res.json())
+    .then((json)=>{
+        allIssue=json.data;
+        displaydata(allIssue);
+    });
+};
+const displaydata=(allbtns)=>{
+    const allDiv = document.getElementById("allDiv");
+    allDiv.innerHTML="";
+
+   
+    for(let allbtn of allbtns)
+    {
+
+     //cardborder in top 
+        let cardborder="";
+        if(allbtn.status==="closed")
+        {
+            cardborder="border-t-6 border-purple-500"
+        }
+        else
+        {
+            cardborder="border-t-6 border-green-500"
+        }
+    //label button part
+         let labelBtn="";
+
+    for (let label of allbtn.labels) {
+        let labelLower = label.toLowerCase();
+      if (labelLower==="good first issue") {
+        continue;
+      }
+      else if (labelLower==="enhancement") {
+        labelBtn+=`
+        <button class="btn btn-soft btn-success border border-green-500 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+      else if(labelLower==="bug"){
+        labelBtn+=`
+        <button class="btn btn-soft btn-secondary border border-pink-700 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+      else if(labelLower==="documentation"){
+        labelBtn+=`
+        <button class="btn btn-soft btn-info border border-blue-400 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+      else{
+        labelBtn+=`
+        <button class="btn btn-soft btn-warning border border-yellow-700 rounded-full">
+        ${label}
+        </button>
+        `;
+      }
+    }
+    //priority button part
+       let priorityLower = allbtn.priority.toLowerCase();
+       let priorityBtn = "";
+
+        if (priorityLower === "low") {
+              priorityBtn = `<button class="btn btn-soft border border-gray-500 rounded-full">${allbtn.priority}</button>`;
+           }
+        else if (priorityLower === "high") {
+             priorityBtn = `<button class="btn btn-soft btn-secondary border border-pink-700 rounded-full">${allbtn.priority}</button>`;
+            }
+        else if (priorityLower === "medium") {
+             priorityBtn = `<button class="btn btn-soft btn-warning border border-yellow-700 rounded-full">${allbtn.priority}</button>`;
+            }
+      //html added
+        const all=document.createElement("div");
+        all.innerHTML=`
+           <div class="bg-slate-100 h-full flex flex-col max-w-[350px] md:max-w-[300px] lg:max-w-[256px] p-5 rounded-xl space-y-4 mx-auto mt-3 ${cardborder}">
+            <div class="flex justify-end">
+                ${priorityBtn}
+            </div>
+            <h4 class="text-xl font-medium">${allbtn.title}</h4>
+            <p class="flex-grow font-thin">${allbtn.description}</p>
+            <div class="flex gap-2 mb-4">
+              <div class="flex gap-3 mb-4 flex-wrap">${labelBtn}</div>
+            </div>
+            <hr class="text-gray-300 mb-5">
+            <p class="text-gray-400 text-xs mb-3">#1 by john_doe</p>
+            <p class="text-gray-400 text-xs">1/15/2024</p>
+        </div>
+        `
+        allDiv.append(all);
+    }
+}
+// open clicked function 
+function showOpen(){
+    const openBtn=allIssue.filter(allbtn=>allbtn.status==="open");
+    all.classList.remove("activecolour");
+    closed.classList.remove("activecolour");
+    open.classList.add("activecolour");
+    displaydata(openBtn);
+}
+// all clicked function 
+function showAll(){
+    displaydata(allIssue);
+
+}
+//closed clicked function
+function showClosed(){
+    const openBtn=allIssue.filter(allbtn=>allbtn.status==="closed");
+    all.classList.remove("activecolour");
+    open.classList.remove("activecolour");
+    closed.classList.add("activecolour");
+
+    displaydata(openBtn);
+}
+//main function
+loadData();
+
